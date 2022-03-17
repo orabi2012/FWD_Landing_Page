7bcd92b8acc7a6f0f91c8e27aa9629151f5017be
Ahmed Orabi <aorabi@outlook.com>
2022-03-17 15:58:26 +0300

    add images

--- js/app.js
+++ js/app.js
@@ -94,7 +94,7 @@ function addSection(index) {
   <div class="landing__container">
     <h2>Section ${index}</h2>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt.</p>
     <img src="./img/${index}.png" alt="FWD" >
     <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
   </div>
@@ -205,7 +205,7 @@ myBtnAdd.addEventListener("click", function () {
 
     addSection(_counter);
     myBtnAdd.innerHTML = `Add Section  (${_counter} of 10 )`;
-    myBtnRemove.innerHTML = `Remove section ${_counter}`;
+    myBtnRemove.innerHTML = `Remove sec #${_counter}`;
 
     Check_counter();
   }
